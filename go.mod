04980a7a943a6e7f1c2062f0e95304cd977939ac
muh.farhan983 <muh.farhan983@gmail.com>
2025-02-12 10:02:08 +0700

    fix: handle cors in frontend

--- go.mod
+++ go.mod
@@ -3,14 +3,19 @@ module microservice
 go 1.23.2
 
 require (
+	github.com/gin-contrib/cors v1.7.3
+	github.com/gin-gonic/gin v1.10.0
+	gorm.io/driver/mysql v1.5.7
+	gorm.io/gorm v1.25.12
+)
+
+require (
 	filippo.io/edwards25519 v1.1.0 // indirect
 	github.com/bytedance/sonic v1.12.8 // indirect
 	github.com/bytedance/sonic/loader v0.2.3 // indirect
 	github.com/cloudwego/base64x v0.1.5 // indirect
-	github.com/cloudwego/iasm v0.2.0 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.8 // indirect
 	github.com/gin-contrib/sse v1.0.0 // indirect
-	github.com/gin-gonic/gin v1.10.0 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator/v10 v10.24.0 // indirect
@@ -20,6 +25,7 @@ require (
 	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/cpuid/v2 v2.2.9 // indirect
+	github.com/kr/text v0.2.0 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
@@ -34,6 +40,4 @@ require (
 	golang.org/x/text v0.22.0 // indirect
 	google.golang.org/protobuf v1.36.5 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
-	gorm.io/driver/mysql v1.5.7 // indirect
-	gorm.io/gorm v1.25.12 // indirect
 )
--- main.go
+++ main.go
@@ -4,12 +4,24 @@ import (
 	"microservice/config"      // Import package untuk konfigurasi database
 	"microservice/controllers" // Import controller yang menangani request
 	"github.com/gin-gonic/gin" // Import Gin sebagai framework web
+	"github.com/gin-contrib/cors" // Tambahkan ini
+	"time"
 )
 
 func main() {
 	config.ConnectDB() // Koneksi ke database sebelum server berjalan
 
 	r := gin.Default() // Inisialisasi router Gin
+
+	// Middleware CORS
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:5173"}, // Hanya izinkan permintaan dari frontend yang berjalan di localhost:5173
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"}, // Mengizinkan metode HTTP yang boleh digunakan
+		AllowHeaders:     []string{"Origin", "Content-Type", "Accept", "Authorization"}, // Header yang diizinkan dalam permintaan
+		ExposeHeaders:    []string{"Content-Length"}, // Header yang boleh diakses oleh frontend setelah permintaan berhasil
+		AllowCredentials: true, // Mengizinkan penggunaan cookie dan header Authorization untu request lintas origin
+		MaxAge:           12 * time.Hour, // Menyimpan izin akses selama 12 jam agar browser tidak perlu mengecek ulang setiap kali mengirim request
+	}))
 
 	r.POST("/article", controllers.CreateArticle) // Rute untuk membuat artikel baru (POST)
 	r.GET("/article", controllers.GetArticles) // Rute untuk mengambil daftar artikel (GET)
